285b7f38be7a142f055c8518b2bfacae3d3ef987
ThaiBinh <binhnguyen.cnit@gmail.com>
2026-09-13 23:47:21 +0700

    Gio 3 - bai 1 - badge giam gia tren bia sach

--- BookStore/components/BookGridCard.tsx
+++ BookStore/components/BookGridCard.tsx
@@ -11,7 +11,12 @@ interface BookCardProps {
 const BookGridCard = ({ title, author, price, coverUrl }: BookCardProps) => {
   return (
     <View style={styles.girdItem}>
-      <Image style={styles.img} source={{ uri: coverUrl }}></Image>
+      <View style={styles.imageContainer}>
+        <Image style={styles.img} source={{ uri: coverUrl }}></Image>
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeText}>-20%</Text>
+        </View>
+      </View>
       <Text numberOfLines={2} style={{ fontWeight: "bold", marginTop: 6 }}>
         {title}
       </Text>
@@ -32,6 +37,21 @@ const styles = StyleSheet.create({
     width: "100%",
     aspectRatio: 3 / 4,
     borderRadius: 20,
+  },
+  imageContainer: {
+    position: "relative",
+  },
+  badgeContainer: {
+    position: "absolute",
+    top: 6,
+    left: 6,
+  },
+  badgeText: {
+    borderWidth: 1,
+    color: "#fff",
+    backgroundColor: "#d44040",
+    padding: 2,
+    borderRadius: 9,
   },
 });
 
