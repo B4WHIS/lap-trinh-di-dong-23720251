64732299105c2fc4e82b7708b78521dcc80bfaa1
ThaiBinh <binhnguyen.cnit@gmail.com>
2026-09-15 19:22:34 +0700

    Gio 4 - bai 1 - boc the

--- BookStore/App.tsx
+++ BookStore/App.tsx
@@ -14,6 +14,7 @@ export default function App() {
       <Header />
       <ScrollView>
         <CategoryChips />
+        {/*  */}
         <View style={styles.girdContainer}>
           <BookGridCard
             title="Tôi Thấy Hoa Vàng Trên Cỏ Xanh"
